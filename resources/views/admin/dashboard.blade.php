d1435f64e37a73c852eb484fd8fc79f3c50865c5
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-01-12 19:20:03 +0100

    FIX: dashboard.blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,21 +10,7 @@
             <div class="card">
                 <div class="card-header">{{ __('User Dashboard') }}</div>
                 
-                <div>
-                    <button class="btn btn-primary"><a href="{{route('admin.projects.index')}}">Guarda i Progetti</a></button>
-                </div>
-
-                <div>
-                    <button class="btn btn-info text-light"><a href="{{route('admin.projects.create')}}">Crea Progetto</a></button>
-                </div>
-
-                <div>
-                    <button class="btn btn-primary"><a href="{{route('admin.types.index')}}">Lista tipi</a></button>
-                </div>
-
-                <div>
-                    <button class="btn btn-primary"><a href="{{route('admin.technologies.index')}}">Lista tecnologie</a></button>
-                </div>
+                
                 
                 
                 <div class="card-body">
@@ -33,9 +19,26 @@
                         {{ session('status') }}
                     </div>
                     @endif
-                    
-
-                    {{ __('You are logged in!') }}
+                    <div class="mb-5 text-center">
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <button class="btn btn-primary btn-lg"><a href="{{route('admin.projects.index')}}">Lista Progetti</a></button>
+                        </div>
+        
+                        <div>
+                            <button class="btn btn-info text-light btn-lg"><a href="{{route('admin.projects.create')}}">Crea Progetto</a></button>
+                        </div>
+        
+                        <div>
+                            <button class="btn btn-primary btn-lg"><a href="{{route('admin.types.index')}}">Lista tipi</a></button>
+                        </div>
+        
+                        <div>
+                            <button class="btn btn-primary btn-lg"><a href="{{route('admin.technologies.index')}}">Lista tecnologie</a></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
